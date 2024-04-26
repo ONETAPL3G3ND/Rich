c6eb19d5e9f1a6fc0f61ee8d21131b141e8ef3e8
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-04-26 19:26:57 +0200

    Add files via upload

---
+++ StyledTextOutput.py
@@ -0,0 +1,7 @@
+from rich.console import Console
+
+if __name__ == "__main__":
+    console = Console()
+    console.print("This is [bold red]bold red[/bold red] text!")
+    console.print("This is [italic green]italic green[/italic green] text!")
+    console.print("This is [underline blue]underlined blue[/underline blue] text!")
